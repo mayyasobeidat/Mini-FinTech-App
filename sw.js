6d21508b593e69d027bb73e177038f3761fe5196
15 - Mayyas Obeidat <115452417+mayyasobeidat@users.noreply.github.com>
2026-08-16 11:51:47 +0200

    Bypass fetch for Google Apps Script requests
    Add condition to bypass fetch event for Google Apps Script requests

--- sw.js
+++ sw.js
@@ -1,3 +1,8 @@
 self.addEventListener('fetch', (event) => {
+  // إذا كان الطلب موحّهاً لـ Google Apps Script، اتكـه للمتصفح ليتعامل معه مباشرة
+  if (event.request.url.includes('script.google.com')) {
+    return; 
+  }
+
   event.respondWith(fetch(event.request));
 });
